e454353b1c27473f1a3d249a4e0d2866e035b344
twoshots02 <twoshots02@gmail.com>
2025-02-17 16:49:37 -0600

    Refactor window class to Window and add methods for redrawing and closing the window

--- window.py
+++ window.py
@@ -1,10 +1,10 @@
 from tkinter import Tk, BOTH, Canvas
 
-class window:
+class Window:
 
     def __init__(self, width, height):
         """
-        Initializes the window with the specified width and height.
+        Initializes the Window with the specified width and height.
 
         Args:
             width (int): The width of the window.
@@ -17,16 +17,34 @@ class window:
             is_running (bool): A flag indicating whether the window is running.
             canvas (Canvas): The canvas widget for drawing in the window.
         """
-        #The constructor should take a width and height
+        # The constructor should take a width and height.
         self.width = width
         self.height = height
-        #It should create a new root widget using Tk() and save it as a data member
+        # It should create a new root widget using Tk() and save it as a data member.
         self.__root = Tk() 
-        #Set the title property of the root widget
+        # Set the title property of the root widget.
         self.__root.title("Maze Generator")
-
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.is_running = False
         self.canvas = Canvas(self.__root, width=self.width, height=self.height)
         self.canvas.pack()
-
+    def redraw(self):
+        """
+        Redraws the window by updating the idle tasks and the window itself.
+        """
+        self.__root.update_idletasks()
+        self.__root.update()
     
+    def wait_for_close(self):
+        """
+        Waits for the window to close by continuously redrawing it.
+        """
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+    
+    def close(self):
+        """
+        Closes the window by setting the is_running flag to False.
+        """
+        self.is_running = False
